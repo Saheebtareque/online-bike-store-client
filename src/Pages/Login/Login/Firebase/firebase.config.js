5e356561fa1a8851bf7f24b82e15ec335dc861b2
saheeb_tareque <saheebtareque97@gmail.com>
2021-11-15 02:25:48 +0600

    firebase initialization is done for the project

---
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAYmn8Z1sanXbaZybmjrQgu1rIZlcLKw6o",
+    authDomain: "online-bike-store.firebaseapp.com",
+    projectId: "online-bike-store",
+    storageBucket: "online-bike-store.appspot.com",
+    messagingSenderId: "866710476711",
+    appId: "1:866710476711:web:87ec015c57d44e26868882"
+};
+export default firebaseConfig; 
+
+
+
+// apiKey: process.env.REACT_APP_API_KEY,
+// authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+// projectId: process.env.REACT_APP_PROJECT_ID,
+// storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+// messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// appId: process.env.REACT_APP_APP_ID,
